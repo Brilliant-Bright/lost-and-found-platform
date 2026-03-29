524904335455c58db6ea55837e52f841865a6ab5
Brilliant Bright <brilliantbrightbbb@gmail.com>
2026-03-29 20:44:22 +0300

    Fixed backend URL typo

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "http://https://national-registry-api.onrender.com:8000";
+const BACKEND_URL = "https://national-registry-api.onrender.com";
 
 let currentRoom = "";
 let currentRole = ""; 
